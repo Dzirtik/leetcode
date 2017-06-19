7997e70782c977e3e32e1bf936847a7a7dea1727
scherbinin <scherbinin@MacBook-Air-support.local>
2017-06-19 15:08:14 +0200

    https://leetcode.com/problems/two-sum/

---
+++ 001_two_sum.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	hash := make(map[int]int)
+	for key, value := range nums {
+		x := target - value
+		if key2, ok := hash[x]; ok {
+			return []int{key2, key}
+		} else {
+			hash[value] = key
+		}
+	}
+	return []int{0, 0}
+}
+
+func main() {
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+}
