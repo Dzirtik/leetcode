a6eef6773230890194426424bd3ea0cbab60f625
Dzirtik <dzirtik@gmail.com>
2017-06-23 14:09:51 +0200

    https://leetcode.com/problems/diagonal-traverse/#/description

---
+++ 498_findDiagonalOrder.go
@@ -0,0 +1,61 @@
+package main
+
+import "fmt"
+
+func findDiagonalOrder(matrix [][]int) []int {
+	var x, y int
+	var result []int
+	var down bool
+
+	fmt.Println(len(matrix))
+
+	if len(matrix) == 0 {
+		return result
+	}
+
+	maxX, maxY := len(matrix[0])-1, len(matrix)-1
+
+	for {
+		result = append(result, matrix[y][x])
+
+		if x == maxX && y == maxY {
+			return result
+		}
+
+		if !down && x == maxX {
+			y++
+			down = down != true
+		} else if down && y == maxY {
+			x++
+			down = down != true
+		} else if down && x == 0 {
+			y++
+			down = down != true
+		} else if !down && y == 0 {
+			x++
+			down = down != true
+		} else {
+			if down {
+				x--
+				y++
+			} else {
+				x++
+				y--
+			}
+		}
+	}
+}
+
+func main() {
+	values := [][]int{}
+
+	row1 := []int{1, 2, 3}
+	row2 := []int{4, 5, 6}
+	row3 := []int{7, 8, 9}
+
+	values = append(values, row1)
+	values = append(values, row2)
+	values = append(values, row3)
+
+	fmt.Println(findDiagonalOrder(values))
+}
