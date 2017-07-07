87227c754153389bfa0b6052d4da37159b500f59
Dzirtik <dzirtik@gmail.com>
2017-07-07 19:04:49 +0300

    25 second atempt

--- 25_reverseKGroup/reverseKGroup.go
+++ 25_reverseKGroup/reverseKGroup.go
@@ -5,47 +5,23 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func revert(node *ListNode, k int) (*ListNode, *ListNode, *ListNode, bool) {
-	if k > 1 {
-		if node.Next != nil {
-			head, prev, last, finished := revert(node.Next, k-1)
-			if finished {
-				prev.Next = node
-				node.Next = nil
-			} else {
-				head = node
-			}
-			return head, node, last, finished
-		}
-		return node, node, nil, false
-	}
-	return node, node, node.Next, true
-}
-
 func reverseKGroup(head *ListNode, k int) *ListNode {
-	if head == nil {
-		return nil
+	curr := head
+	var count int
+	for curr != nil && count != k {
+		curr = curr.Next
+		count++
 	}
-	var first, mid, last, prev, result *ListNode
-	var finished bool
-	for {
-		first, mid, last, finished = revert(head, k)
-		if result == nil {
-			result = first
-		} else {
-			if finished {
-				prev.Next = first
-			} else {
-				prev.Next = head
-			}
-
+	if count == k {
+		curr = reverseKGroup(curr, k)
+		for count > 0 {
+			count--
+			tmp := head.Next
+			head.Next = curr
+			curr = head
+			head = tmp
 		}
-		if last == nil {
-			break
-		}
-		prev = mid
-		head = last
+		head = curr
 	}
-
-	return result
+	return head
 }
